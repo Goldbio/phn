50e9f738e21554d5b0fd3e7ba2810d8f6f525025
Goldbio <cwkeum@gmail.com>
2013-07-19 12:17:38 +0900

    Added displayNews function to MainActivity.java

--- src/com/geference/MainActivity.java
+++ src/com/geference/MainActivity.java
@@ -1,22 +1,28 @@
 package com.geference;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	private String fileContent;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	
+		// Get content 
 		GetNews news = new GetNews();
 		news.execute();
 		while(true){
 			if( news.checkDone() == true){
-				String fileContent = news.getContent();
-				Toast.makeText(this, fileContent,0).show();
+				fileContent = news.getContent();
+				
 				break;
 			}
 			try{
@@ -24,11 +30,30 @@ public class MainActivity extends Activity {
 			}catch(Exception e){}
 			
 		}
+	
+		// Display
+		
+		displayNews(fileContent);
+		
 		
 	}
 
-	
+	protected void displayNews(String json){
+		try {
+			JSONArray newsJson = new JSONArray(json);
+			for(int i=0;i<newsJson.length(); i++){
+				JSONObject eachNews = newsJson.getJSONObject(i);
+				String publisher =eachNews.getString("publisher") ;
+				Toast.makeText(this, publisher,0).show();
+				
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 
-	
 }
 
